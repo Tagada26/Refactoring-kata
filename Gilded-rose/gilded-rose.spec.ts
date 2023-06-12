779c3e246992c0036492f157922a0caf4811d90a
Marjorie <aubertmarjorie2@gmail.com>
2023-06-12 11:56:53 +0200

    Test and refacto

--- Gilded-rose/gilded-rose.spec.ts
+++ Gilded-rose/gilded-rose.spec.ts
@@ -1,4 +1,6 @@
-import { GildedRose, Item } from "./gilded-rose";
+import { OldGildedRose, Item } from "./old-gilded-rose";
+import { GildedRose } from "./gilded-rose";
+import { itemsList } from "./golden-master-text-test";
 
 
 describe('Gilded Rose', () => {
@@ -8,3 +10,57 @@ describe('Gilded Rose', () => {
     expect(items[0].name).toBe('foo');
   });
 });
+
+describe('Gilded Rose golden master', () => {
+  it('should foo', () => {
+    const gildedRose = new OldGildedRose([new Item('foo', 0, 0)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].name).toBe('foo');
+  });
+  it.each(itemsList) ('should update quality as expected for $name' , (item) => {
+    //Given
+    const oldGildedRose = new OldGildedRose([{...item}]);
+    const gildedRose = new GildedRose([{...item}]);
+    //when
+    const oldItems = oldGildedRose.updateQuality();
+    const items = gildedRose.updateQuality();
+    //then
+    expect(items).toEqual(expect.arrayContaining(oldItems));
+  })
+  it.each(itemsList) ('should update quality as expected for $name' , (item) => {
+    //Given
+    const oldGildedRose = new OldGildedRose([{...item}]);
+    const gildedRose = new GildedRose([{...item}]);
+    //when
+    let days: number = 1000;
+
+    for (let i = 0; i < days; i++) {
+      const oldItems = oldGildedRose.updateQuality();
+      const items = gildedRose.updateQuality();
+      //then
+      expect(items).toEqual(expect.arrayContaining(oldItems));
+    }
+  })
+});
+
+
+describe('Item is immutable', () => {
+  // Nous n'avons pas le droit de toucher à la classe item 
+  const item = new Item('hello', 0, 0);
+  //Name, Sellin et quality
+  const properties = Object.keys(item);
+
+  it('should there 3 properties', () => {
+    expect(properties.length).toEqual(3)
+  })
+  it('should the first property is name', ()=> {
+    expect(properties[0]).toEqual('name');
+  })
+  it('sould the second property is sellIn', ()=> {
+      expect(properties[1]).toEqual('sellIn');
+  })
+  it('sould the third property is quality', ()=> {
+    expect(properties[2]).toEqual('quality');
+  })
+})
+
--- Gilded-rose/golden-master-text-test.ts
+++ Gilded-rose/golden-master-text-test.ts
@@ -1,6 +1,18 @@
-import { Item, GildedRose } from './gilded-rose';
+import { Item, OldGildedRose } from './old-gilded-rose';
 
-const items = [
+export const items = [
+  new Item("+5 Dexterity Vest", 10, 20), //
+  new Item("Aged Brie", 2, 0), //
+  new Item("Elixir of the Mongoose", 5, 7), //
+  new Item("Sulfuras, Hand of Ragnaros", 0, 80), //
+  new Item("Sulfuras, Hand of Ragnaros", -1, 80),
+  new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
+  new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49),
+  new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49),
+  // this conjured item does not work properly yet
+  new Item("Conjured Mana Cake", 3, 6)];
+
+export const itemsList = [
   new Item("+5 Dexterity Vest", 10, 20), //
   new Item("Aged Brie", 2, 0), //
   new Item("Elixir of the Mongoose", 5, 7), //
@@ -13,9 +25,10 @@ const items = [
   new Item("Conjured Mana Cake", 3, 6)];
 
 
-const gildedRose = new GildedRose(items);
+const oldGildedRose = new OldGildedRose(items);
 
 let days: number = 2;
+console.log(process.argv); //?
 if (process.argv.length > 2) {
     days = +process.argv[2];
   }
@@ -28,5 +41,5 @@ for (let i = 0; i < days; i++) {
 
   });
   console.log();
-  gildedRose.updateQuality();
+  oldGildedRose.updateQuality();
 }
