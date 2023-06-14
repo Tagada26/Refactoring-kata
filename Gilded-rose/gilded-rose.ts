4332e204b23dd0cfa7c0b2d43496cb6707daa177
Marjorie <aubertmarjorie2@gmail.com>
2023-06-14 09:45:34 +0200

    step refacto

--- Gilded-rose/gilded-rose.ts
+++ Gilded-rose/gilded-rose.ts
@@ -23,47 +23,40 @@ export class GildedRose {
   // Peut devenir statique.  
   updateQuality() {
     for (let i = 0; i < this.items.length; i++) {
-
       const item = this.items[i];
 
-      if(item.name === 'Sulfuras, Hand of Ragnaros') {
+      if (item.name === 'Sulfuras, Hand of Ragnaros') {
         continue
       }
 
-      if (item.name != 'Aged Brie' && item.name != 'Backstage passes to a TAFKAL80ETC concert') {
-        if (item.quality > 0) {
-          item.quality = item.quality - 1
-        }
-      } else {
+      if (item.name == 'Aged Brie' || item.name == 'Backstage passes to a TAFKAL80ETC concert') {
         if (item.quality < 50) {
           item.quality = item.quality + 1
           if (item.name == 'Backstage passes to a TAFKAL80ETC concert') {
-            if (item.sellIn < 11) {
-              if (item.quality < 50) {
-                item.quality = item.quality + 1
-              }
+            if (item.quality < 50 && item.sellIn < 11 ) {
+              item.quality = item.quality + 1
             }
-            if (item.sellIn < 6) {
-              if (item.quality < 50) {
-                item.quality = item.quality + 1
-              }
+            if (item.quality < 50 && item.sellIn < 6) {
+              item.quality = item.quality + 1
             }
           }
+        }
+      } else {
+        if (item.quality > 0) {
+          item.quality = item.quality - 1
         }
       }
       item.sellIn = item.sellIn - 1;
       if (item.sellIn < 0) {
-        if (item.name != 'Aged Brie') {
-          if (item.name != 'Backstage passes to a TAFKAL80ETC concert') {
-            if (item.quality > 0) {
-              item.quality = item.quality - 1
-            }
-          } else {
-            item.quality = 0
-          }
-        } else {
+        if (item.name == 'Aged Brie' ) {
           if (item.quality < 50) {
             item.quality = item.quality + 1
+          }
+        } else if (item.name == 'Backstage passes to a TAFKAL80ETC concert')  {
+            item.quality = 0
+        } else {
+          if (item.quality > 0) {
+            item.quality = item.quality - 1
           }
         }
       }
