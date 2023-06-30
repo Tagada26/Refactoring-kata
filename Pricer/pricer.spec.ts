9ba2f31f58dd7e8925d82df64c94634291560baa
Marjorie <aubertmarjorie2@gmail.com>
2023-06-30 14:05:50 +0200

    pricer

--- Pricer/pricer.spec.ts
+++ Pricer/pricer.spec.ts
@@ -22,7 +22,7 @@ describe("Pricer implementation", () => {
     //then 
     expect(articlePrice).toEqual('3.63 €')
   });
-  it.skip("should return the price with taxes for x articles from a sentence", () => {
+  it("should return the price with taxes for x articles from a sentence", () => {
     //given 
     const article = '3 articles à 1,21 € et taxe 5 %' 
 
@@ -31,5 +31,35 @@ describe("Pricer implementation", () => {
 
     //then 
     expect(articlePrice).toEqual('3.81 €')
+  });
+  it("should return the price with taxes for x articles from a sentence", () => {
+    //given 
+    const article = '3 articles à 1,21 € et taxe 20 %' 
+
+    //when 
+    const articlePrice = getPricer(article)
+
+    //then 
+    expect(articlePrice).toEqual('4.36 €')
+  });
+  it("should apply 3% discounts when HT price is > 1000euros", () => {
+    //given 
+    const article = '5 articles à 345,00 € et taxe 10 %' 
+
+    //when 
+    const articlePrice = getPricer(article)
+
+    //then 
+    expect(articlePrice).toEqual('1840.58 €')
+  });
+  it("should apply 5% discounts when HT price is > 5000euros", () => {
+    //given 
+    const article = '5 articles à 1299,00 € et taxe 10 %' 
+
+    //when 
+    const articlePrice = getPricer(article)
+
+    //then 
+    expect(articlePrice).toEqual('6787.28 €')
   });
 });
--- Pricer/pricer.ts
+++ Pricer/pricer.ts
@@ -7,10 +7,24 @@ export const getPricer = (sentence: string): string => {
     const taxe = Number(words[7])
   
 
-    const totalHT = numberOfArticle * priceForOneArticle 
-    const taxePart = totalHT * (taxe/100) 
-    const totalTTC = totalHT + taxePart 
+    const totalHTbeforeDiscount = numberOfArticle * priceForOneArticle 
+    if(totalHTbeforeDiscount > 5000) {
+        const totalHTAfterDiscount = totalHTbeforeDiscount * 0.95
+        const taxePart = totalHTAfterDiscount * (taxe/100) 
+        const totalTTC = Math.round((totalHTAfterDiscount + taxePart ) * 100)
+
+       return `${totalTTC / 100} ${device}`
+    } 
+    if(totalHTbeforeDiscount > 1000 && totalHTbeforeDiscount <= 5000 ) {
+        const totalHTAfterDiscount = totalHTbeforeDiscount * 0.97
+        const taxePart = totalHTAfterDiscount * (taxe/100) 
+        const totalTTC = Math.round((totalHTAfterDiscount + taxePart ) * 100)
+
+       return `${totalTTC / 100} ${device}`
+    } 
+    const taxePart = totalHTbeforeDiscount * (taxe/100) 
+    const totalTTC = Math.round((totalHTbeforeDiscount + taxePart ) * 100)
 
 
-    return `${totalTTC} ${device}`
+    return `${totalTTC / 100} ${device}`
 }
