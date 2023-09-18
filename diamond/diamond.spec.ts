abb900f1a65817096bb84f06cdc870b25db2fe4a
Marjorie <aubertmarjorie2@gmail.com>
2023-09-18 14:11:19 +0200

    refacto in progress
    Co-authored-by: basttran <basttran@users.noreply.github.com>

--- diamond/diamond.spec.ts
+++ diamond/diamond.spec.ts
@@ -2,9 +2,11 @@ const A = 65;
 const buildDiamond = (seed: string) => {
     const seedPosition = seed.charCodeAt(0) - 'A'.charCodeAt(0); //?
     let result = seed === 'A' ? 'A' : 'A\n';
+
+    //let diamond = []
     
     for(let i = 1; i <= seedPosition; i++) {
-        result += getLetter(i) + getLetter(i) + '\n'; 
+        result += getLetter(i) + ' '.repeat(i) + getLetter(i) + '\n'; 
     }
 
     for(let i = (seedPosition - 1) ; i >= 1 ; i--) {
@@ -25,19 +27,28 @@ describe('build diamond', () => {
         expect(result).toEqual(`A`)
     });
 
-    it('should build diamond with proper letters', () => {
+    it('should build diamond with proper letters and space', () => {
         // when 
         const result = buildDiamond('C')
 
         //then
         const lines = result.split('\n')
         expect(lines[0]).toEqual(`A`);
-        expect(lines[1]).toEqual(`BB`);
-        expect(lines[2]).toEqual(`CC`);
+        expect(lines[1]).toEqual(`B B`);
+        expect(lines[2]).toEqual(`C  C`);
         expect(lines[3]).toEqual(`BB`);
+        expect(lines[4]).toEqual(`A`);
     });
+
 })
 
 function getLetter(i: number) {
     return String.fromCharCode(A + i);
 }
+
+
+// expect(lines[0]).toEqual(`  A`);
+// expect(lines[1]).toEqual(` B B`);
+// expect(lines[2]).toEqual(`C   C`);
+// expect(lines[3]).toEqual(` B B`);
+// expect(lines[4]).toEqual(`  A`);
